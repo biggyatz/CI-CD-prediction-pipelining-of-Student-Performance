8e11265b9b3241f115d4e7fb8ec972892fc3c437
biggyat <bigyatz123@gmail.com>
2024-06-08 11:15:41 +0545

    data ingestion

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -5,3 +5,45 @@ from src.logger import logging
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
+
+# the data class will directly define the data class a
+@dataclass
+class DataIngestionConfig:
+    train_data_path: str =os.path.join('artifacts',"train.csv") # this is the output path of the data ingestion model
+    test_data_path: str =os.path.join('artifacts',"test.csv")
+    raw_data_path: str =os.path.join('artifacts',"data.csv")
+
+# now we create the actual data ingestion that knows the inputs already from the 
+# DataIngestionConfig file path
+class DataIngestion:
+    # step 1 is to call for the inputs saved in the DIC config file artifacts
+    def __init__(self):
+        self.ingestion_config=DataIngestionConfig()
+    def initiate_data_ingestion(self):
+        # if the database is stored in some databases to read from mongodb client we need
+        # to write in the utils.py
+        logging.info("Entered the data ingestion method or component")
+        try:
+            # just change the import to the user specific data source mongo or other sources 
+            df=pd.read_csv("notebook\data\stud.csv")
+            logging.info("Data import sucessful")
+
+            os.makedirs(os.path.dirname(self.ingestion_config.train_data_path),exist_ok=True)
+            df.to_csv(self.ingestion_config.raw_data_path,index=False,header=True)
+            logging.info("Train test split initiated")
+            train_set,test_set=train_test_split(df,test_size=0.2,random_state=42)
+            train_set.to_csv(self.ingestion_config.train_data_path,index=False,header=True)
+            test_set.to_csv(self.ingestion_config.test_data_path,index=False,header=True)
+            logging.info("Ingestion of the data is completed")
+            return(
+                self.ingestion_config.train_data_path,
+                self.ingestion_config.test_data_path
+            )# we pass these two informations as this will be the input for the next model 
+            # data transformation take the info from them and start their respective process
+
+        except Exception as e:
+            raise CustomException(e,sys)
+
+if __name__=="__main__":
+    obj=DataIngestion()
+    obj.initiate_data_ingestion()
